6521136267d11061b89994f323271d0e9003fa55
Piyush Kumar Sahu <Piyush.Kumar.Sahu@ibm.com>
2026-05-29 01:12:37 +0530

    feat: add outputs.tf for Valkey ICD module
    Port from terraform-ibm-icd-redis with the following key changes:
    - Update resource references from redis_database to valkey_database
    - Update connection data source attribute from rediss to valkey

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,61 @@
-########################################################################################################################
+##############################################################################
 # Outputs
-########################################################################################################################
+##############################################################################
 
-#
-# Developer tips:
-#   - Below are some good practise sample outputs
-#   - They should be updated for outputs applicable to the module being added
-#   - Use variable validation when possible
-#
+output "id" {
+  description = "Valkey instance id"
+  value       = ibm_database.valkey_database.id
+}
 
-output "account_id" {
-  description = "An alpha-numeric value identifying the account ID."
-  value       = ibm_resource_instance.cos_instance.account_id
+output "version" {
+  description = "Valkey instance version"
+  value       = ibm_database.valkey_database.version
 }
 
 output "guid" {
-  description = "The GUID of the resource instance."
-  value       = ibm_resource_instance.cos_instance.guid
-}
-
-output "id" {
-  description = "The unique identifier of the resource instance."
-  value       = ibm_resource_instance.cos_instance.id
+  description = "Valkey instance guid"
+  value       = ibm_database.valkey_database.guid
 }
 
 output "crn" {
-  description = "The CRN of the resource instance."
-  value       = ibm_resource_instance.cos_instance.crn
+  description = "Valkey instance crn"
+  value       = ibm_database.valkey_database.resource_crn
+}
+
+output "service_credentials_json" {
+  description = "Service credentials json map"
+  value       = local.service_credentials_json
+  sensitive   = true
+}
+
+output "service_credentials_object" {
+  description = "Service credentials object"
+  value       = local.service_credentials_object
+  sensitive   = true
+}
+
+output "cbr_rule_ids" {
+  description = "CBR rule ids created to restrict Valkey"
+  value       = module.cbr_rule[*].rule_id
+}
+
+output "adminuser" {
+  description = "Database admin user name"
+  value       = ibm_database.valkey_database.adminuser
+}
+
+output "hostname" {
+  description = "Database connection hostname"
+  value       = data.ibm_database_connection.database_connection.valkey[0].hosts[0].hostname
+}
+
+output "port" {
+  description = "Database connection port"
+  value       = data.ibm_database_connection.database_connection.valkey[0].hosts[0].port
+}
+
+output "certificate_base64" {
+  description = "Database connection certificate"
+  value       = data.ibm_database_connection.database_connection.valkey[0].certificate[0].certificate_base64
+  sensitive   = true
 }
