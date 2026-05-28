2c964e6801521a009b630d051e8943832c9171af
Piyush Kumar Sahu <Piyush.Kumar.Sahu@ibm.com>
2026-05-29 01:13:08 +0530

    feat: add examples/basic/main.tf for Valkey module
    Port from terraform-ibm-icd-redis basic example:
    - Rename redis_version to valkey_version
    - Remove service_endpoints (hardcoded to private in the module)
    - Update service credential names and endpoints to valkey/private

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,31 +1,52 @@
-########################################################################################################################
-# Resource group
-########################################################################################################################
+##############################################################################
+# Resource Group
+##############################################################################
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.6.0"
+  version = "1.6.1"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
 
-########################################################################################################################
-# COS
-########################################################################################################################
+##############################################################################
+# Valkey
+##############################################################################
 
-#
-# Developer tips:
-#   - Call the local module / modules in the example to show how they can be consumed
-#   - include the actual module source as a code comment like below so consumers know how to consume from correct location
-#
-
-module "cos" {
+module "database" {
   source = "../.."
   # remove the above line and uncomment the below 2 lines to consume the module from the registry
-  # source            = "terraform-ibm-modules/<replace>/ibm"
-  # version           = "X.Y.Z" # Replace "X.Y.Z" with a release version to lock into a specific release
-  name              = "${var.prefix}-cos"
-  resource_group_id = module.resource_group.resource_group_id
-  resource_tags     = var.resource_tags
+  # source  = "terraform-ibm-modules/icd-valkey/ibm"
+  # version = "X.Y.Z" # Replace "X.Y.Z" with a release version to lock into a specific release
+  resource_group_id   = module.resource_group.resource_group_id
+  name                = "${var.prefix}-valkey"
+  region              = var.region
+  valkey_version      = var.valkey_version
+  access_tags         = var.access_tags
+  tags                = var.resource_tags
+  member_host_flavor  = var.member_host_flavor
+  deletion_protection = false
+  service_credential_names = [
+    {
+      name     = "valkey_admin"
+      role     = "Administrator"
+      endpoint = "private"
+    },
+    {
+      name     = "valkey_operator"
+      role     = "Operator"
+      endpoint = "private"
+    },
+    {
+      name     = "valkey_viewer"
+      role     = "Viewer"
+      endpoint = "private"
+    },
+    {
+      name     = "valkey_editor"
+      role     = "Editor"
+      endpoint = "private"
+    }
+  ]
 }
