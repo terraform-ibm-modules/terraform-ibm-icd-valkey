3b4a9481897493982d8420440567ebe2e5e6a0c9
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2026-07-07 19:47:35 +0530

    update test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testschematic"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
 
 // Use existing resource group
@@ -14,57 +14,47 @@ const resourceGroup = "geretain-test-valkey"
 const basicExampleDir = "examples/basic"
 const advancedExampleDir = "examples/advanced"
 
-// Valkey Gen2 only supports private service endpoints, so tests must run via
-// IBM Cloud Schematics (which executes inside IBM Cloud's network).
-
-func setupOptions(t *testing.T, prefix string, dir string) *testschematic.TestSchematicOptions {
-	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
-		Testing: t,
-		Prefix:  prefix,
-		TarIncludePatterns: []string{
-			"*.tf",
-			dir + "/*.tf",
-		},
-		TemplateFolder:         dir,
-		Tags:                   []string{"test-schematic"},
-		DeleteWorkspaceOnFail:  false,
-		WaitJobCompleteMinutes: 90,
+func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:       t,
+		TerraformDir:  dir,
+		Prefix:        prefix,
+		Region:        "eu-de", // Currently, Valkey is supported only in the eu-de region.
+		ResourceGroup: resourceGroup,
 	})
-
-	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
-		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
-		{Name: "prefix", Value: options.Prefix, DataType: "string"},
-		{Name: "region", Value: "eu-de", DataType: "string"},
-		{Name: "resource_group", Value: resourceGroup, DataType: "string"},
-		{Name: "member_host_flavor", Value: "bx3d.4x20", DataType: "string"},
-	}
 	return options
 }
 
-func TestRunBasicExampleSchematics(t *testing.T) {
+// Consistency test for the basic example
+func TestRunBasicExample(t *testing.T) {
 	t.Parallel()
 
 	options := setupOptions(t, "valkey-basic", basicExampleDir)
-	err := options.RunSchematicTest()
+
+	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
 }
 
-func TestRunAdvancedExampleSchematics(t *testing.T) {
+func TestRunAdvancedExample(t *testing.T) {
 	t.Parallel()
 
 	options := setupOptions(t, "valkey-adv", advancedExampleDir)
-	err := options.RunSchematicTest()
+
+	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
 }
 
-func TestRunUpgradeExampleSchematics(t *testing.T) {
+// Upgrade test (using advanced example)
+func TestRunUpgradeExample(t *testing.T) {
 	t.Parallel()
 
 	options := setupOptions(t, "valkey-adv-upg", advancedExampleDir)
-	options.CheckApplyResultForUpgrade = true
 
-	err := options.RunSchematicUpgradeTest()
+	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
 		assert.Nil(t, err, "This should not have errored")
+		assert.NotNil(t, output, "Expected some output")
 	}
 }
