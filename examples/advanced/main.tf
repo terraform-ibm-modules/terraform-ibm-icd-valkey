37e087cd9804dd7b6c96b06d76c1f669233e5f54
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-09-05 08:50:41 +0530

    fix(deps): update dependencies (#12)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -20,7 +20,7 @@ locals {
 
 module "key_protect_all_inclusive" {
   source            = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version           = "5.6.5"
+  version           = "5.6.6"
   resource_group_id = module.resource_group.resource_group_id
   # Note: Database instance and Key Protect must be created in the same region when using BYOK
   # See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-key-protect&interface=ui#key-byok
